ce2c1be75c78f95b8a0b325f533ac5372d59517d
akirattii <tanaka.akira.2006@gmail.com>
2018-02-23 13:43:59 +0900

    tiny bugfix

--- api-server/middleware/error-handler.js
+++ api-server/middleware/error-handler.js
@@ -1,8 +1,8 @@
 module.exports = function(err, req, res, next) {
-	if (err) {
-  	logger.error("!!!!! errorHandler middleware caught error:", err, err.stack);
-		logger.error("reset gex.isTxBusy to false");
-  	gex.isTxBusy = false;
+  if (err) {
+    logger.error("!!!!! errorHandler middleware caught error:", err, err.stack);
+    logger.error("reset gex.isTxBusy to false");
+    gex.isTxBusy = false;
   }
   res.writeHead(500, {});
   res.write("Internal Server Error");
